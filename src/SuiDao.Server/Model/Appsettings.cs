1c9ac93e4d4a65a2e7a7b654462e48123da3b5b3
SpringHgui <740360381@qq.com>
2020-07-01 17:55:30 +0800

    init project

---
+++ src/SuiDao.Server/Model/Appsettings.cs
@@ -0,0 +1,13 @@
+﻿using FastTunnel.Core.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuiDao.Server
+{
+    public class Appsettings
+    {
+        public ServerConfig ServerSettings { get; set; }
+    }
+}
