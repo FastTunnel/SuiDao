166e6093185e9d8f904fdecf5d03c65f69e6b9fb
SpringHgui <740360381@qq.com>
2020-08-25 14:46:20 +0800

    使用.net core后台任务方式启动server

--- src/SuiDao.Server/Program.cs
+++ src/SuiDao.Server/Program.cs
@@ -1,80 +1,36 @@
-﻿using FastTunnel.Core.Config;
-using FastTunnel.Core.Core;
+﻿using FastTunnel.Core.Core;
 using FastTunnel.Core.Handlers;
 using FastTunnel.Core.Handlers.Server;
-using FastTunnel.Core.Host;
 using FastTunnel.Core.Logger;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using NLog;
-using System;
-using System.IO;
-using System.Threading;
+using Microsoft.Extensions.Hosting;
+using NLog.Extensions.Logging;
 
 namespace SuiDao.Server
 {
     public class Program
     {
-        static Appsettings appsettings;
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            LogManager.Configuration = NlogConfig.getNewConfig();
-            var logger = LogManager.GetCurrentClassLogger();
-            logger.Debug("===== FastTunnel Server Start =====");
-
-            try
-            {
-                var servicesProvider = new Host().Config(Config).Build();
-                Run(servicesProvider);
-            }
-            catch (Exception ex)
-            {
-                // NLog: catch any exception and log it.
-                logger.Error(ex);
-                Console.WriteLine(ex);
-            }
-            finally
-            {
-                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-                LogManager.Shutdown();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
-        private static ServerConfig implementationFactory(IServiceProvider arg)
-        {
-            if (appsettings == null)
-            {
-                var conf = new ConfigurationBuilder()
-                  .SetBasePath(Directory.GetCurrentDirectory())
-                  .AddJsonFile("appsettings.json", false, true)
-                  .Build();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((context) =>
+                {
+                    context.AddNLog(NlogConfig.getNewConfig());
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<SuiDaoServer>();
 
-                appsettings = conf.Get<Appsettings>();
-            }
-
-            return appsettings.ServerSettings;
-        }
-
-        private static void Config(ServiceCollection service)
-        {
-            service.AddSingleton<FastTunnelServer>()
-                .AddSingleton<ServerConfig>(implementationFactory)
-                .AddSingleton<LoginHandler>() 
-                .AddSingleton<SwapMsgHandler>()
-                .AddSingleton<HeartHandler>()
-                .AddSingleton<IConfigHandler, SuiDaoConfigHandler>();
-        }
-
-        private static void Run(IServiceProvider servicesProvider)
-        {
-            var server = servicesProvider.GetRequiredService<FastTunnelServer>();
-            server.Run();
-
-            while (true)
-            {
-                Thread.Sleep(10000 * 60);
-            }
-        }
+                    // DI
+                    services.AddSingleton<FastTunnelServer>()
+                        .AddSingleton<LoginHandler>()
+                        .AddSingleton<SwapMsgHandler>()
+                        .AddSingleton<HeartHandler>()
+                        .AddSingleton<IConfigHandler, SuiDaoConfigHandler>();
+                });
     }
 }
---
+++ src/SuiDao.Server/Service/SuiDaoServer.cs
@@ -0,0 +1,55 @@
+﻿using FastTunnel.Core.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuiDao.Server
+{
+    public class SuiDaoServer : IHostedService
+    {
+        ILogger<SuiDaoServer> _logger;
+        IConfiguration _configuration;
+        FastTunnelServer _fastTunnelServer;
+
+        public SuiDaoServer(ILogger<SuiDaoServer> logger, IConfiguration configuration, FastTunnelServer fastTunnelServer)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _fastTunnelServer = fastTunnelServer;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("===== SuiDao Server Starting =====");
+
+            try
+            {
+                Run();
+            }
+            catch (Exception ex)
+            {
+                // NLog: catch any exception and log it.
+                _logger.LogError(ex, "Server Error");
+                Console.WriteLine(ex);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private void Run()
+        {
+            _fastTunnelServer.Run(_configuration.Get<Appsettings>().ServerSettings);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("===== SuiDao Server Stoping =====");
+            return Task.CompletedTask;
+        }
+    }
+}
