b61b2b076eb9fb6966ae5e2fbfa1cf1596f651d3
SpringHgui <740360381@qq.com>
2021-06-18 22:52:08 +0800

    1

--- src/SuiDao.Server/Program.cs
+++ src/SuiDao.Server/Program.cs
@@ -3,7 +3,6 @@ using FastTunnel.Core.Global;
 using FastTunnel.Core.Handlers;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NLog.Web;
 using System;
 
 namespace SuiDao.Server
@@ -12,22 +11,7 @@ namespace SuiDao.Server
     {
         public static void Main(string[] args)
         {
-            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-            try
-            {
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception exception)
-            {
-                //NLog: catch setup errors
-                logger.Error(exception, "Stopped program because of exception");
-                throw;
-            }
-            finally
-            {
-                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-                NLog.LogManager.Shutdown();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -41,12 +25,11 @@ namespace SuiDao.Server
                 })
                 .ConfigureLogging((HostBuilderContext context, ILoggingBuilder logging) =>
                 {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(LogLevel.Trace);
-                    //ILoggingBuilder.AddFilter("System", LogLevel.Warning);
-                    //ILoggingBuilder.AddFilter("Microsoft", LogLevel.Warning);
-                    //ILoggingBuilder.AddLog4Net();
+                    //logging.ClearProviders();
+                    //logging.SetMinimumLevel(LogLevel.Trace);
+
+                    logging.AddLog4Net();
+                    logging.SetMinimumLevel(LogLevel.Debug);
                 });
-                //.UseNLog();
     }
 }
