65850da811cb9bade2b290cd61e209b9e55c2640
ioxygen <springhgui@outlook.com>
2021-07-09 15:32:26 +0800

    使用log4net日志组件

--- src/SuiDao.Client/Program.cs
+++ src/SuiDao.Client/Program.cs
@@ -3,8 +3,6 @@ using FastTunnel.Core.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NLog;
-using NLog.Web;
 using SuiDao.Client.Models;
 using System;
 
@@ -14,22 +12,7 @@ namespace SuiDao.Client
     {
         public static void Main(string[] args)
         {
-            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-            try
-            {
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception exception)
-            {
-                //NLog: catch setup errors
-                logger.Error(exception, "Stopped program because of exception");
-                throw;
-            }
-            finally
-            {
-                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-                LogManager.Shutdown();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -47,8 +30,8 @@ namespace SuiDao.Client
                 .ConfigureLogging((HostBuilderContext context, ILoggingBuilder logging) =>
                 {
                     logging.ClearProviders();
-                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                })
-                .UseNLog();  // NLog: Setup NLog for Dependency injection
+                    logging.AddLog4Net();
+                    logging.SetMinimumLevel(LogLevel.Debug);
+                });
     }
 }
