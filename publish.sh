1d9499b90a9db97dfd1f69749596475c72d740c8
SpringHgui <740360381@qq.com>
2021-07-10 23:38:28 +0800

    update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf publish/*
-projects=("SuiDao.Client" "SuiDao.Server")
+projects=("SuiDao.Client") # "SuiDao.Server"
 plates=("win-x64" "win-arm" "osx-x64" "linux-arm" "linux-x64")
 for project in ${projects[*]}; do
     echo
@@ -10,12 +10,12 @@ for project in ${projects[*]}; do
         echo "plate=${plate}"
         echo src/$project/$project.csproj
         dotnet publish src/$project/$project.csproj -o=publish/$project.$plate -c=release -r=$plate -p:PublishSingleFile=true -p:PublishTrimmed=true --nologo
-        cp src/$project/appsettings.json publish/$project.$plate
+        # cp src/$project/appsettings.json publish/$project.$plate
         echo
         echo "=========开始打包 ========="
         echo
         cd publish && tar -zcvf $project.$plate.tar.gz $project.$plate
         cd ../
-        rm -rf publish/$project.$plate
+        # rm -rf publish/$project.$plate
     done
 done
